ea19101efb354055dcd7a616a45290a0aab1fdbd
lvillegas21 <lautaro.villegas.com@gmail.com>
2021-09-20 16:23:46 -0300

    desafio clase 12 firebase

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -3,27 +3,29 @@ import ItemDetail from '../ItemDetail/ItemDetail'
 import { db } from '../ItemCollection/ItemCollection';
 
 const ItemDetailContainer =({match})=>{
-    console.log(match)
+    console.log(match.params.id)
     const [items,setItems]=useState()
+    
+
 
     useEffect(()=>{
-        
         const ItemCollection=db.collection('zapatillaz')
-        const item=ItemCollection.doc()
-       
-        item.get().then((doc)=>{
-            if(!doc.exists){
+        const item=ItemCollection.doc(match.params.id)
+        
+        item.get()
+            .then((doc)=>{
+                if(!doc.exists){
                 console.log('no funciona')
                 return
             }
             console.log('funciona')
             setItems({id:doc.id,...doc.data()})
         })       
-    },[])
+    },[match])
   
     return(
         <div>
-            <ItemDetail data={items}/>    
+            {items && <ItemDetail data={items}/>  }   
         </div>
     )
 }
